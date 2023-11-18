7833164a201458242bccee8764364e1ec4c43782
iloozealot <iloozealot@gmail.com>
2023-11-18 15:39:50 -0600

    Add project files.

---
+++ HandlingExceptions/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+//int[] ages = new int[] { 1, 3, 5 };
+
+//for (int i = 0; i <= ages.Length; i++)
+//{
+//	try
+//	{
+//        Console.WriteLine(ages[i]);
+//    }
+//	catch (Exception ex)
+//	{
+//        Console.WriteLine("We had an error.");
+//        Console.WriteLine(ex.Message);
+//        throw;
+//    }
+//}
+
+//Console.ReadLine();
+
+int[] ages = new int[] { 0, 1, 2, 3, 4 };
+
+for (int i = 0; i <= ages.Length; i++)
+{
+	try
+	{
+		Console.WriteLine(ages[i]);
+    }
+	catch (Exception ex)
+	{
+        Console.WriteLine("We had an error.");
+		Console.WriteLine(ex.Message);
+	}
+}
